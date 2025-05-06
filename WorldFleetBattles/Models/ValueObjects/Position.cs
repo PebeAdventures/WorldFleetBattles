21df7636c43666eb47f903d631e20033d286286b
piotr <piotr@PJB>
2025-05-06 20:54:23 +0200

    Add ValueObject Position and implement base HumanPlayer

---
+++ WorldFleetBattles/Models/ValueObjects/Position.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldFleetBattles.Models.ValueObjects
+{
+    public readonly struct Position : IEquatable<Position>
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Position(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"({X}, {Y})";
+
+        public override bool Equals(object? obj)
+            => obj is Position other && Equals(other);
+
+        public bool Equals(Position other)
+            => X == other.X && Y == other.Y;
+
+        public override int GetHashCode()
+            => HashCode.Combine(X, Y);
+
+        public static bool operator ==(Position left, Position right)
+            => left.Equals(right);
+
+        public static bool operator !=(Position left, Position right)
+            => !left.Equals(right);
+    }
+}
--- WorldFleetBattles/Services/Players/HumanPlayer.cs
+++ WorldFleetBattles/Services/Players/HumanPlayer.cs
@@ -8,8 +8,15 @@ namespace WorldFleetBattles.Services.Players
 {
     internal class HumanPlayer : PlayerBase
     {
-        public HumanPlayer(string name) : base(name)
+        private readonly IInputProvider _input;
+        public HumanPlayer(string name, IInputProvider input) : base(name)
         {
+            _input = input;
+        }
+        public override bool TakeTurn(IPlayer opponent)
+        {
+            var target = _input.GetTargetPosition();
+            return Attack(opponent, target);
         }
     }
 }
