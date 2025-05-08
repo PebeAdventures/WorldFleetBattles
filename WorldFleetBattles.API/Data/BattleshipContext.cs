fde94a4a17c3e0f1ad51516c5c739835b6b8a57d
Piotr Bujak <pi0tr.buj4k@gmail.com>
2025-05-08 14:26:04 +0200

    add API project and MVC project. add basic API project configuration

---
+++ WorldFleetBattles.API/Data/BattleshipContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldFleetBattles.Models.GameBoard;
+
+namespace WorldFleetBattles.API.Data
+{
+    public class BattleshipContext : DbContext
+    {
+        public BattleshipContext(DbContextOptions<BattleshipContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ship> Ships { get; set; }
+
+    }
+}
